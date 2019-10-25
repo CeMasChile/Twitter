3f570ee4845eb10f0ed6c99db8587fda58e789ad
Tomás Rojas <tomas.rojas.c@ug.uchile.cl>
2019-10-24 22:32:51 -0300

    Probando Geopandas con las regiones de Chile y subiendo las formas necesarias para graficar

--- codigo/GeoPlot.py
+++ codigo/GeoPlot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import geopandas as gpd
 
-Chile = gpd.read_file('./Comunas/comunas.shp')
+Chile = gpd.read_file('./Regiones/Regional.shp')
 
 Chile.plot()
 plt.show()
