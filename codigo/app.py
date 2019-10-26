8cf37a05d8fbf556a469c13bc040f52155993e38
Tomás Rojas <tomas.rojas.c@ug.uchile.cl>
2019-10-26 15:47:17 -0300

    App funcional, solo hay que darle el nombre del archivo donde se están escribiendo los datos. Eso es lo que hay que hacer automático de alguna manera perdón si mi código no es muy lindo uwu, voy a amononarlo cuando pueda, o si alguien puede, es libre de hacerlo

--- codigo/app.py
+++ codigo/app.py
@@ -5,7 +5,7 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
 
-
+direction = './OutputStreaming_20191026-153306.csv'
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -14,7 +14,6 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_csv(direction)
 
-available_indicators = df['created_at'].unique()
 
 app.layout = html.Div([
     dcc.Graph(id='hist'),
@@ -32,18 +31,16 @@ app.layout = html.Div([
     [Input('interval', 'n_intervals')]
 )
 def update_graph(n):
-    df.update(pd.read_csv(direction))
+    df = pd.read_csv(direction)
 
 
-    return {
+    data = {
         'data': [go.Histogram(
             x=df['created_at']
         )]
     }
 
+    return go.Figure(data)
+
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-
-
