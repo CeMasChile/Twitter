4246efb27aafd53d29d0d0ad98be0d48071f14db
Tomás Rojas <tomas.rojas.c@ug.uchile.cl>
2019-10-27 15:05:46 -0300

    arreglos app

--- codigo/app.py
+++ codigo/app.py
@@ -4,11 +4,9 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
-<<<<<<< HEAD
 
 # direction of the css file
 direction = './OutputStreaming_20191026-153306.csv'
-=======
 from main import get_keywords
 import numpy as np
 
@@ -93,11 +91,6 @@ app.layout = html.Div([
     dropdown_menu,
     figure,
     # interval in milliseconds to update the figure
-    dcc.Interval(
-        id='interval',
-        interval=1 * 1000,  # in milliseconds
-        n_intervals=0
-    )
     time_interval
 ])
 
@@ -110,12 +103,15 @@ app.layout = html.Div([
 # how to update the figure
 def update_graph(n):        # no sé pq está esa 'n' ahí, pero no la saquen que si no no funciona
     # update a pandas DataFrame
-    df = pd.read_csv(direction)
+
+    data = getDf2plot(direction)
 
     # assign the 'created_at' column to the histogram
     data = {
-        'data': [go.Histogram(
-            x=df['created_at']
+        'data': [go.Scatter(
+            x=data['date'][1:],     # se salta el primer elemento porque no es el minuto completo
+            y=data['freq'][1:],
+            mode='lines+markers'
         )]
     }
 
