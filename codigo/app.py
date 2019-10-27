2703457e3dfd2c8a988d2d874fc79fb5cdf94588
stepp1 <sfaragg@gmail.com>
2019-10-27 16:51:05 +0000

    Revert "Arreglo algunas cosas"
    This reverts commit b95d4d4020ee0560222999bdc59bbfbfd6760812.

--- codigo/app.py
+++ codigo/app.py
@@ -4,23 +4,9 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
-import numpy as np
-
-
-def getDf2plot(direction):
-    df = pd.read_csv(direction)
-    DF = pd.to_datetime(df['created_at']).dt.floor('min')
-    max_date = DF.max()
-    DF = pd.to_datetime(DF.loc[DF < max_date])
-    DF = DF.sort_index().value_counts()
-    data = {'date': DF.index, 'freq': DF.values}
-    data = pd.DataFrame(data).sort_values('date')
-    return data
-
-
 
 # direction of the css file
-direction = './OutputStreaming_20191026-225744.csv'
+direction = './OutputStreaming_20191026-153306.csv'
 
 # css
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -35,7 +21,7 @@ app.layout = html.Div([
     # interval in milliseconds to update the figure
     dcc.Interval(
         id='interval',
-        interval=20 * 1 * 1000,  # in milliseconds
+        interval=1 * 1000,  # in milliseconds
         n_intervals=0
     )
 ])
@@ -49,15 +35,12 @@ app.layout = html.Div([
 # how to update the figure
 def update_graph(n):
     # update a pandas DataFrame
-
-    data = getDf2plot(direction)
+    df = pd.read_csv(direction)
 
     # assign the 'created_at' column to the histogram
     data = {
-        'data': [go.Scatter(
-            x=data['date'][1:],     # se salta el primer elemento porque no es el minuto completo
-            y=data['freq'][1:],
-            mode='lines+markers'
+        'data': [go.Histogram(
+            x=df['created_at']
         )]
     }
 
