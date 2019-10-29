c856439e79502be126b21ee6e72249c61e99b462
Victor Faraggi <sfaragg@gmail.com>
2019-10-29 16:06:46 -0300

    some changes

--- codigo/app.py
+++ codigo/app.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import numpy as np
-import io
+
 from wordcloud import WordCloud
 from PIL import Image
 import plotly.graph_objs as go
